d654aa96aadf0ea9420d822033cc6d0e0df70f87
alk <amilcar.muratet@gmail.com>
2020-08-04 14:17:28 +0200

    [alk] test gitignore context.xml

--- src/fr/eni/AuctioWebapp/servlets/Accueil.java
+++ src/fr/eni/AuctioWebapp/servlets/Accueil.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Accueil")
 public class Accueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages-user/Accueil.jsp");
 		rd.forward(request, response);
